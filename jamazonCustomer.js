9e5269a6785d3f8f31a8a6d8c12ca137dd89b61f
rdo <ryan@libertylearner.com>
2017-12-17 16:42:14 -0800

    setup the printing of all the products. Not tabbed correctly after the product name, but it works

--- jamazonCustomer.js
+++ jamazonCustomer.js
@@ -1,3 +1,4 @@
+//setup requires
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
@@ -12,16 +13,8 @@ var connection = mysql.createConnection({
 
 connection.connect( function(err){
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
+    //console.log('connected as id ' + connection.threadId);
 ;})
-
-
-connection.query('SELECT * FROM jamazon_inventory WHERE item_id=2',function(err, res){
-    if (err) throw err;
-    console.log(res)
-
-})
-
 
 
 
@@ -29,21 +22,65 @@ connection.query('SELECT * FROM jamazon_inventory WHERE item_id=2',function(err,
 
 
 //lists all the products and their information
-function listProducts(){
+function listAllProducts(){
+    //hard-coded for 10
+    //build a simple tab separated tabled
+    console.log('Welcome to the Jamazon Web Store.\n')
+
     for(var i=1;i<11;i++){
         connection.query('SELECT * FROM jamazon_inventory WHERE item_id='+i,function(err, res){
             if (err) throw err;
-            console.log(res)
+            console.log(JSON.stringify(res))
+
         
         })
     }
 
 }
 
-listProducts();
-
-//given the selection, return the quantity
-function getInventory(){
+listAllProducts();
 
 
+//7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+// * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
+function processInventory(){
+
+
+}
+
+
+
+
+//asks user for the quantity desired
+function requestQuantity(){
+    //analogous to chosenItemID in requestItemID
+    var chosenQuantity;
+
+
+
+    validateQuantity();
+}
+
+
+
+//utilizes inquire to ask for the item ID.
+function requestItemID(){
+    //going to write an inquire prompt that gets the prompt
+    var chosenItemID;
+
+
+
+
+    connection.query('SELECT * FROM jamazon_inventory WHERE item_id='+chosenItemID,
+    function(err,res){
+        if (err) throw err;
+
+    })
+
+
+
+    requestQuantity();
 }
